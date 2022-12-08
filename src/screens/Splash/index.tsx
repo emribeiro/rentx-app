66776d31a492ac2056b803e98b0241f3565c8648
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2022-12-08 10:29:55 -0300

    chore [Splash Screen]: Navigate to home when animation ends

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,11 +1,17 @@
 import { Container } from "./styles";
 import BrandSvg from '../../assets/brand.svg';
 import LogoSvg from '../../assets/logo.svg';
-import Animated, { Extrapolate, interpolate, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
+import Animated, { Extrapolate, interpolate, runOnJS, useAnimatedStyle, useSharedValue, withTiming } from "react-native-reanimated";
 import { useEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 export function Splash(){
     let animationValue = useSharedValue(0);
+    const navigation = useNavigation();
+
+    function navigateToHome(){
+        navigation.navigate('Home' as never);
+    }
 
     const brandAnimation = useAnimatedStyle(() => {
         return {
@@ -32,7 +38,12 @@ export function Splash(){
     useEffect(() => {
         animationValue.value = withTiming(50, {
             duration: 1000
-        })
+        },
+        () => {
+            'worklet'
+            runOnJS(navigateToHome)();
+        } 
+        )
     }, []);
 
     return (
