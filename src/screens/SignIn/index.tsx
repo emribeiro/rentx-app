678d0a03982f7daadc3e8f30d0e4044cfa9eaacb
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2023-01-11 10:23:38 -0300

    chore [SignIn Screen]: inputText accessibility

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,5 @@
 
-import { StatusBar } from 'react-native'
+import { Keyboard, KeyboardAvoidingView, StatusBar } from 'react-native'
 import { Button } from '../../components/Button'
 import { useTheme } from 'styled-components';
 import {
@@ -12,51 +12,57 @@ import {
 } from './styles'
 import { Input } from '../../components/Input';
 import { PasswordInput } from '../../components/PasswordInput';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 
 export function SignIn(){
     const theme = useTheme();
 
     return (
-        <Container>
-            <StatusBar
-                barStyle='dark-content'
-                translucent
-            />
-            <Header>
-                <Title>
-                    Estamos {'\n'}
-                    quase lá
-                </Title>
-                <SubTitle>
-                    Faça seu login para começar {'\n'}
-                    uma experiência incrível.
-                </SubTitle>
-            </Header>
+        <KeyboardAvoidingView behavior='position' enabled>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Container>
+                    <StatusBar
+                        barStyle='dark-content'
+                        translucent
+                    />
+                    <Header>
+                        <Title>
+                            Estamos {'\n'}
+                            quase lá
+                        </Title>
+                        <SubTitle>
+                            Faça seu login para começar {'\n'}
+                            uma experiência incrível.
+                        </SubTitle>
+                    </Header>
 
-            <Form>
-                <Input
-                    iconName='mail'
-                    placeholder='E-mail'
-                    keyboardType='email-address'
-                />
-                <PasswordInput
-                    iconName='lock'
-                    placeholder='Senha'
-                />
-            </Form>
-            <Footer>
-                <Button 
-                    name='Login'
-                    enabled={false}
-                    style={{marginBottom: 8}}
-                />
-                <Button 
-                    name='Criar conta gratuita'
-                    enabled={false}
-                    color={theme.colors.white}
-                    light={true}
-                />
-            </Footer>
-        </Container>
+                    <Form>
+                        <Input
+                            iconName='mail'
+                            placeholder='E-mail'
+                            keyboardType='email-address'
+                            autoCorrect={false}
+                        />
+                        <PasswordInput
+                            iconName='lock'
+                            placeholder='Senha'
+                        />
+                    </Form>
+                    <Footer>
+                        <Button 
+                            name='Login'
+                            enabled={false}
+                            style={{marginBottom: 8}}
+                        />
+                        <Button 
+                            name='Criar conta gratuita'
+                            enabled={false}
+                            color={theme.colors.white}
+                            light={true}
+                        />
+                    </Footer>
+                </Container>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView> 
     )
 }
--- src/screens/SignIn/styles.ts
+++ src/screens/SignIn/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components/native";
 import {getStatusBarHeight} from "react-native-iphone-x-helper"
 
 export const Container = styled.View`
-    flex: 1;
     margin: 0 32px;
 `;
 
